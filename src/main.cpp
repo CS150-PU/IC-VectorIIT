e809186ff6586e51cdc99d600da82bc17e9acb17
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2024-12-02 17:37:15 +0000

    Finalized template

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,20 @@
 //******************************************************************************
 // File name:   main.cpp
-// Author:      CS Faculty
-// Date:        xx/xx/xxxx (Replace xx/xx/xxxx with month/day/year)
-// Class:       CS150-xx (Replace xx with your section number)
-// Assignment:  Hello World
-// Purpose:     Display the message Hello World to the screen
-// Hours:       0.25
+// Author:      
+// Date:        
+// Class:       
+// Assignment:  
+// Purpose:     
+// Hours:       
 //******************************************************************************
 
 #include <iostream>
 #include <bits/stdc++.h>
 #include <vector>
+#include <iomanip>
 
 // TODO #2: Write a function prototype called getRandom that has two
-//          parameters first and second. if first <= second, return a random
+//          parameters first and second. If first <= second, return a random
 //          integer in the range of first to second inclusive; otherwise,
 //          return a random integer in the range of second to first inclusive.
 //          As an example, a call to getRandom (5, 10); returns a random
@@ -25,9 +26,10 @@
 //          aVector with 10 random values in the range of 2 and 100 inclusive.
 
 // TODO $7: Write a function printVector that accepts an integer vector and 
-//          outputs each element of the vector one value per line. As an
-//          example, a call to printVector (aVector); will print each
-//          value in the vector one per line.
+//          outputs each element of the vector num values per line
+//          in a field of fieldWidth spaces. As an example, a call to 
+//          printVector (aVector, 3, 5); will print 3 values per line with
+//          each value in the vector printed right justified in a field of 5.
 
 using namespace std;
 
@@ -36,16 +38,18 @@ int main () {
 
   srand (0);  // sets the seed value to 0, do not change this
 
-  // TODO #6: Fill the vector aVector with 10 random velues between
+  // TODO #6: Fill the vector aVector with 13 random velues between
   //          5 and 1000 inclusive by calling the function fillVector.
 
-  // TODO #9: Call printVector to output the contents of aVector
+  // TODO #9: Call printVector to output the contents of aVector using the
+  //          call printVector (aVector, 3, 5);
   
   return EXIT_SUCCESS;
 }
 
-// TODO #3: Write the definition for getRandom as described in TODO #1
+// TODO #3: Write the definition for getRandom as described in TODO #2
 
-// TODO #5: Write the definition for fillVector as described in TODO #4
+// TODO #5: Write the definition for fillVector as described in TODO #4. This
+//          function is to call the function getRandom.
 
 // TODO #8: Write the definition for printVector as described in TODO $7
